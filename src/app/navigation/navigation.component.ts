3f629d51d97bba48fa61b929163f36b5f8cd7308
pokuong lao <pokuonglao123@gmail.com>
2024-06-04 09:35:32 -0700

    1

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive,NgClass],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
-
+  @Input() showNavigation: boolean = false;
+  constructor() {}
+  toggleNavigation() {
+    this.showNavigation = !this.showNavigation;
+  }
+  
 }
