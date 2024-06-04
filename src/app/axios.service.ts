5783dd96cb939b5a0934482831c427f91c41a9f3
pokuong lao <pokuonglao123@gmail.com>
2024-06-04 10:59:04 -0700

    event

--- src/app/axios.service.ts
+++ src/app/axios.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AxiosService {
-  
+  constructor() {
+    axios.defaults.baseURL = "http://localhost:8080";
+    axios.defaults.headers['Content-Type'] = "application/json";
+  }
 }
