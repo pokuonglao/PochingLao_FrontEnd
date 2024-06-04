ea241af11cdd298ef84a2b8a9949aa4f8798e9e8
pokuong lao <pokuonglao123@gmail.com>
2024-06-04 11:02:12 -0700

    event

--- src/app/axios.service.ts
+++ src/app/axios.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
+/**
+ * The AxiosService class provides a centralized service for making HTTP requests
+ * using Axios in an Angular application.
+ */
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // This service will be available application-wide.
 })
 export class AxiosService {
+  private axiosInstance: AxiosInstance; // Axios instance to handle HTTP requests
+
+  /**
+   * Constructor initializes the Axios instance with default configuration.
+   */
   constructor() {
-    axios.defaults.baseURL = "http://localhost:8080";
-    axios.defaults.headers['Content-Type'] = "application/json";
+    this.axiosInstance = axios.create({
+      baseURL: "http://localhost:8080", // Set the base URL for Axios requests
+      headers: {
+        'Content-Type': 'application/json' // Default content type for requests
+      }
+    });
   }
 }
