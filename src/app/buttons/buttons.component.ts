98d7ea2ecfccf8664dcbf31a2f47ada5e4a87575
pokuong lao <pokuonglao123@gmail.com>
2024-06-04 10:00:04 -0700

    events

--- src/app/buttons/buttons.component.ts
+++ src/app/buttons/buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-buttons',
@@ -8,6 +8,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './buttons.component.css'
 })
 export class ButtonsComponent {
-  @Output() loginEvent = new EventEmitter;
-  @Output() logoutEvent = new EventEmitter;
+
 }
