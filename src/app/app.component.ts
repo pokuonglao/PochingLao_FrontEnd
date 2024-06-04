4f1489d1f99e37bbbedd6eca8ad81998638a577a
pokuong lao <pokuonglao123@gmail.com>
2024-06-04 09:34:16 -0700

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component'; 
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavigationComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PochingLao';
+  showNavigation: boolean = false;
+
+  toggleNavigation(): void {
+    this.showNavigation = !this.showNavigation;
+  }
 }
