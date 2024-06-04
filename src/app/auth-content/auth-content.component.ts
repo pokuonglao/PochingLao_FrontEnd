60374d3ff22a947368c5a4311c5b44774215c5d4
pokuong lao <pokuonglao123@gmail.com>
2024-06-04 09:42:50 -0700

    auth-content

--- src/app/auth-content/auth-content.component.ts
+++ src/app/auth-content/auth-content.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AxiosService } from '../axios.service';
+import { NgFor } from '@angular/common'; 
+import { LoginFormComponent } from '../login-form/login-form.component';
 
 @Component({
   selector: 'app-auth-content',
   standalone: true,
-  imports: [],
+  imports: [ NgFor,LoginFormComponent],
   templateUrl: './auth-content.component.html',
-  styleUrl: './auth-content.component.css'
+  styleUrls: ['./auth-content.component.css']
 })
-export class AuthContentComponent {
+export class AuthContentComponent implements OnInit {
+  data: string[] = [];
 
+  constructor(private axiosService: AxiosService) { }
+
+  ngOnInit(): void {
+    this.axiosService.request(
+      "GET",
+      "/messages",
+      {}
+    ).then(
+      (response) => this.data = response.data
+    );
+  }
 }
