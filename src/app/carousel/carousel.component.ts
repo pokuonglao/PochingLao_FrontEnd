cf377b2a394dbf16036e17abec92cda0a13f89b5
pokuong lao <pokuonglao123@gmail.com>
2024-06-14 16:33:43 -0700

    minor updates

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [NgbCarouselModule,CommonModule],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent {
+   // Define your images array with actual data
+   images = [
+    { url: 'IMG_4076 (1).jpg', alt: 'Image 2 Alt Text', label: '', description: 'Poching and her peers' },
+    { url: 'IMG_4080 (1).jpg', alt: 'Image 3 Alt Text', label: '', description: 'Poching caring for patient' },
+    { url: 'IMG_4081 (1).jpg', alt: 'Image 1 Alt Text', label: '', description: 'Poching taking data for patient' },
+    { url: 'IMG_4086 (1).jpg', alt: 'Image 2 Alt Text', label: '', description: 'Poching keeping a child company' },
+    { url: 'IMG_4088 (1).jpg', alt: 'Image 3 Alt Text', label: '', description: 'Poching taking data for patient' }
+  ];
+}
