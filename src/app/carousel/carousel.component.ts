b0d491aef2ca8bc5f511ddcfbea7b6982cc6aa4d
pokuong lao <pokuonglao123@gmail.com>
2024-06-14 17:10:11 -0700

    1

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -12,10 +12,10 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 export class CarouselComponent {
    // Define your images array with actual data
    images = [
-    { url: '/IMG_4076.jpg', alt: 'Image 2 Alt Text', label: '', description: 'Poching and her peers' },
-    { url: '/IMG_4080.jpg', alt: 'Image 3 Alt Text', label: '', description: 'Poching caring for patient' },
-    { url: '/IMG_4081.jpg', alt: 'Image 1 Alt Text', label: '', description: 'Poching taking data for patient' },
-    { url: '/IMG_4086.jpg', alt: 'Image 2 Alt Text', label: '', description: 'Poching keeping a child company' },
-    { url: '/IMG_4088.jpg', alt: 'Image 3 Alt Text', label: '', description: 'Poching taking data for patient' }
+    { url: '/IMG4076.jpg', alt: 'Image 2 Alt Text', label: '', description: 'Poching and her peers' },
+    { url: '/IMG4080.jpg', alt: 'Image 3 Alt Text', label: '', description: 'Poching caring for patient' },
+    { url: '/IMG4081.jpg', alt: 'Image 1 Alt Text', label: '', description: 'Poching taking data for patient' },
+    { url: '/IMG4086.jpg', alt: 'Image 2 Alt Text', label: '', description: 'Poching keeping a child company' },
+    { url: '/IMG4088.jpg', alt: 'Image 3 Alt Text', label: '', description: 'Poching taking data for patient' }
   ];
 }
